a481e7364316a57a1ad09cbae768b82307c73d4c
Utkarsh Choudhary <72992574+UDogg@users.noreply.github.com>
2023-06-05 12:33:50 +0530

    cc

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,14 +1,12 @@
 import {
   mobile,
   backend,
-  creator,
   web,
   javascript,
   typescript,
   html,
   css,
   reactjs,
-  redux,
   tailwind,
   nodejs,
   mongodb,
@@ -18,7 +16,6 @@ import {
   meta,
   starbucks,
   tesla,
-  shopify,
   carrent,
   jobit,
   tripguide,
@@ -130,19 +127,6 @@ const experiences = [
     ],
   },
   {
-    title: "Web Developer",
-    company_name: "Shopify",
-    icon: shopify,
-    iconBg: "#383E56",
-    date: "Jan 2022 - Jan 2023",
-    points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
-    ],
-  },
-  {
     title: "Full stack Developer",
     company_name: "Meta",
     icon: meta,
@@ -160,26 +144,26 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
+      "Dummy Testomonial",
+    name: "Jane Doe",
     designation: "CFO",
-    company: "Acme Co",
+    company: "XYZ Co",
     image: "https://randomuser.me/api/portraits/women/4.jpg",
   },
   {
     testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
+      "Dummy Testomonial",
+    name: "John Doe",
     designation: "COO",
-    company: "DEF Corp",
+    company: "ABC Corp",
     image: "https://randomuser.me/api/portraits/men/5.jpg",
   },
   {
     testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
+      "Dummy Testomonial",
+    name: "Human Being",
     designation: "CTO",
-    company: "456 Enterprises",
+    company: "Vought Enterprises",
     image: "https://randomuser.me/api/portraits/women/6.jpg",
   },
 ];
