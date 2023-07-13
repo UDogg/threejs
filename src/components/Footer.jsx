b00d8abe841587cc3099b2f67174af19e45afe18
Utkarsh Choudhary <ufc5009@psu.edu>
2023-07-13 23:11:27 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,18 +9,9 @@ const Footer = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col items-center">
           <p className="text-center text-white">
-            Welcome to our portfolio website! I, Utkarsh, an adept web developer,
-            am delighted to present our collaborative prowess. Joined by Purna Siva Sai,
-            whose astute knowledge of Kubernetes proved invaluable, we have harnessed our expertise
-            to construct an impressive JavaScript-based portfolio site.
-            My deftness in JavaScript is evident in the visually alluring and functionally impeccable website,
-            engendering a superlative user experience. Seamless navigation and interactive elements synergistically
-            weave an engaging tapestry. Purna's mastery of Kubernetes facilitated deployment on a scalable container
-            orchestration platform, adeptly leveraging GitLab's CI/CD pipeline for automated scaling, thus ensuring
-            unwavering performance amidst surges in traffic. Embracing perpetual growth, I ardently pursue emerging
-            technologies to deliver pioneering solutions aligned with evolving industry exigencies.
-            Delve into our projects, including the dynamic portfolio website crafted by me and deployed via
-            Kubernetes by Purna, and envision how our combined prowess can enrich your forthcoming endeavors.
+            Welcome to my portfolio website! My name is Utkarsh (UC) Choudhary. 
+            I am currently a junior in Penn State University, studying Computer Science.
+            My contacts are listed below.
             <a href="https://www.youtube.com/@javascriptmastery" target="_blank"> Credit: Adrian -JavaScript Mastery</a>
           </p>
         </div>
