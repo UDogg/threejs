af3b302d9745fbb79967b26290b33a78734b35d5
Utkarsh Choudhary <ufc5009@psu.edu>
2025-07-30 15:00:53 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,8 +9,8 @@ const Footer = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col items-center">
           <p className="text-center text-white">
-            Welcome to my portfolio website! My name is Utkarsh (UC) Choudhary. 
-            I am currently a rising senior in Penn State University, studying Computer Science.
+            Welcome to my portfolio website! My name is <a href="https://www.linkedin.com/in/utkarsh-choudhary-a46146a4/" target="_blank"Utkarsh Choudhary</a>. 
+            I studied Computer Science at <a href="https://www.psu.edu/" target="_blank">The Pennsylvania State University</a>
             My contacts are listed below.
           </p>
         </div>
@@ -20,25 +20,26 @@ const Footer = () => {
             <div className="flex flex-col items-center">
               <FaHome className="text-white mr-2" size={20} />
               <div>
-                <p className="text-white text-center"><a href="https://www.psu.edu/" target="_blank">Pennsylvania State University</a></p>
-                <p className="text-white text-center"><a href="https://www.google.com/maps/dir//penn+state/" target="_blank">University Park, PA 16802.</a></p>
+                <p className="text-white text-center"><a href="https://www.google.com/maps/place/chembur+east/data=!4m2!3m1!1s0x3be7c5ede708d39d:0xd81ae764fa3f136d?sa=X&ved=1t:155783&ictx=111" target="_blank">Chembur East, Mumbai</a></p>
+                <p className="text-white text-center"><a href="https://www.google.com/maps/place/mumbai/data=!4m2!3m1!1s0x3be7c6306644edc1:0x5da4ed8f8d648c69?sa=X&ved=1t:155783&ictx=111" target="_blank">Mumbai, Maharashtra 400074, India.</a></p>
               </div>
             </div>
             <div className="flex flex-col items-center">
               <FaMailBulk className="text-white mr-2" size={20} />
-              <a href="mailto:ufc5009@psu.edu" className="text-white text-center">ufc5009@psu.edu</a>
+              <a href="mailto:choudharyu2003@gmail.com" className="text-white text-center">choudharyu2003@gmail.com</a>
             </div>
 
             <div className="flex flex-col items-center">
               <FaPhone className="text-white mr-2" size={20} />
-              <h4 className="text-white text-center">+1(484)-797-8693</h4>
+              <h4 className="text-white text-center">+91 8433771419</h4>
             </div>
             <div className="flex flex-col items-center">
               <DiGithubBadge className="text-white mr-2" size={20} />
               <a href="https://www.github.com/udogg" target="_blank" className="text-white text-center">github.com/UDogg</a>
             </div>
             <div className="flex flex-col items-center">
-              <h2>Last Updated: November 20, 2024</h2>
+              <h2>Last Updated: July 30, 2025</h2>
+              <h4>Contact Form intentionally broken.</h4>
             </div>
             <div className="absolute bottom-0 left-0">
               <a href="https://www.youtube.com/@javascriptmastery" target="_blank"> Credit: Adrian -JavaScript Mastery</a>
